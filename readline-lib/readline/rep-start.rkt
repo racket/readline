fbf67382933ee50d0a4445d6f11c4cb38c01ee7d
Asumu Takikawa <asumu@ccs.neu.edu>
2015-03-20 14:22:11 -0400

    Reorganize paren matching additions into a new module
    (to be squashed)

--- readline-lib/readline/rep-start.rkt
+++ readline-lib/readline/rep-start.rkt
@@ -3,8 +3,12 @@
 
 #lang racket/base
 
-(require "pread.rkt")
+(require "pread.rkt"
+         "match-parens.rkt")
 
 ;; Change the input port and readline-prompt hook
 (current-input-port readline-input)
 (current-prompt-read read-cmdline-syntax)
+
+;; Add parenthesis matching
+(install-match-paren-bindings!)
--- readline-lib/readline/rktrl.rkt
+++ readline-lib/readline/rktrl.rkt
@@ -1,13 +1,15 @@
 #lang racket/base
 
-(require ffi/unsafe (only-in '#%foreign ffi-obj)
-         syntax-color/racket-lexer)
+(require ffi/unsafe (only-in '#%foreign ffi-obj))
 (provide readline readline-bytes
          add-history add-history-bytes
          history-length history-get history-delete
          set-completion-function!
          readline-newline readline-redisplay
-         match-paren-timeout)
+         readline-buffer readline-point
+         readline-insert-text
+         readline-bind-key
+         readline-startup-hook)
 
 ;; libncurses and/or libtermcap needed on some platforms
 (void (ffi-lib "libcurses" #:fail (lambda () #f)))
@@ -140,103 +142,30 @@
                  (get-ffi-obj "rl_newline" libreadline (_fun -> _void)))))
 
 ;; force redisplay of prompt and current user input
-(define readline-redisplay
+(define rl-forced-update-display
   (get-ffi-obj "rl_forced_update_display" libreadline (_fun -> _void)))
-
-;; support for bouncing/matching parens, with some inspiration from
-;; Guile's readline support
-
-;; timeout in milliseconds
-(define match-paren-timeout (make-parameter 500))
-
-;; these internal bindings aren't for export
-(define rl-buffer (ffi-obj #"rl_line_buffer" libreadline))
-(define rl-point  (ffi-obj #"rl_point" libreadline))
-
 (define rl-redisplay (get-ffi-obj #"rl_redisplay" libreadline (_fun -> _void)))
-(define rl-bind-key  (get-ffi-obj #"rl_bind_key" libreadline
-                                  (_fun _int (_fun _int _int -> _void)
-                                        -> _void)))
-(define rl-insert    (get-ffi-obj #"rl_insert" libreadline
-                                  (_fun _int _int -> _void)))
 
-;; int? int? -> void?
-;; Matches parentheses in the buffer and flashes the current pair when a
-;; new closing paren is typed. Ignores the first argument and the second
-;; argument should be the key passed from readline.
-(define (match-parens _ char)
-  (define cur-point (ptr-ref rl-point _int))
-  (rl-insert 1 char)
-  (when (match-paren-timeout)
-    (define new-point (find-match cur-point char))
-    (when new-point
-      (ptr-set! rl-point _int new-point)
-      (rl-redisplay)
-      (sleep (/ (match-paren-timeout) 1000))
-      ;; move to after the newly inserted character
-      (ptr-set! rl-point _int (add1 cur-point)))))
+(define (readline-redisplay #:force? [force? #t])
+  (if force?
+      (rl-forced-update-display)
+      (rl-redisplay)))
 
-;; exact-integer? byte? -> (or/c #f exact-integer?)
-;; Find the index in the readline buffer of the matching paren or
-;; #f if it does not exist.
-(define (find-match point char)
-  (define input (buffer->input-port point))
-  (define target-sym (byte->symbol char))
-  (let loop ([stack null] [last-match #f])
-    (define-values (match type paren-kind start end)
-      (racket-lexer input))
-    (cond [(eof-object? match)
-           ;; check that the match, if it exists, is actually a match
-           ;; for the new inserted character (by position in the buffer)
-           (and last-match
-                (eq? target-sym (car (cadr last-match)))
-                ;; the lexer is 1-indexed, so subtract for 0-index
-                (= point (sub1 (cadr (cadr last-match))))
-                (sub1 (cadr (car last-match))))]
-          [(eq? type 'parenthesis)
-           ;; matching pairs are removed from the stack, but remembered
-           ;; for the end in case it's the new character and its match
-           (if (and (not (null? stack))
-                    (matching-paren? (caar stack) paren-kind))
-               (loop (cdr stack)
-                     (list (car stack) `(,paren-kind ,start)))
-               (loop (cons `(,paren-kind ,start) stack)
-                     last-match))]
-          [else (loop stack last-match)])))
+;; insert text into the buffer
+(define readline-insert-text
+  (get-ffi-obj #"rl_insert_text" libreadline (_fun _string -> _void)))
 
-;; symbol? symbol? -> boolean?
-;; Test if two parentheses are a matching pair
-(define (matching-paren? p1 p2)
-  (or (and (eq? p1 '|(|) (eq? p2 '|)|))
-      (and (eq? p1 '|[|) (eq? p2 '|]|))
-      (and (eq? p1 '|{|) (eq? p2 '|}|))))
+;; buffer-related globals
+(define readline-point  (make-c-parameter #"rl_point" libreadline _int))
+(define (readline-buffer)
+  (get-ffi-obj #"rl_line_buffer" libreadline _bytes))
 
-;; byte? -> symbol?
-;; Convert a character code for a parenthesis to a symbol
-(define (byte->symbol byte)
-  (string->symbol (make-string 1 (integer->char byte))))
+;; bind a key to a user-provided operation
+(define readline-bind-key
+  (get-ffi-obj #"rl_bind_key" libreadline
+               (_fun _int (_fun _int _byte -> _void)
+                     -> _void)))
 
-;; exact-integer? -> input-port
-;; Turn the readline buffer contents into an input port from
-;; the start up to the specified point
-(define (buffer->input-port point)
-  (define buffer-string
-    (list->string (for/list ([idx (in-range (add1 point))])
-                    (integer->char (buffer-ref idx)))))
-  (open-input-string buffer-string))
-
-;; exact-integer? -> byte?
-;; Dereference a byte in the readline buffer
-(define (buffer-ref idx)
-  (ptr-ref (ptr-ref rl-buffer _pointer) _byte idx))
-
-;; bind a startup hook to install the paren matching in the right keymap
-(define close-paren-code   (char->integer #\)))
-(define close-bracket-code (char->integer #\]))
-(define close-brace-code   (char->integer #\}))
-
-(set-ffi-obj! "rl_startup_hook" libreadline (_fun -> _void)
-              (lambda ()
-                (rl-bind-key close-paren-code   match-parens)
-                (rl-bind-key close-bracket-code match-parens)
-                (rl-bind-key close-brace-code   match-parens)))
+;; a function to call on startup for readline
+(define readline-startup-hook
+  (make-c-parameter #"rl_startup_hook" libreadline (_fun -> _void)))
